c332dcbf65f2f1ba2b4ed032f94e62f836d1cb15
Jonathan Haas <jonathan.haas@gmail.com>
2014-10-14 22:50:25 -0400

    fixed type

--- my_cookbooks/rjmetrics/metadata.rb
+++ my_cookbooks/rjmetrics/metadata.rb
@@ -5,7 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures rjmetrics example exercise, it creates a database and imports a SQL file (for testing purposes)'
 version          '0.0.1'
 
-#defining dependancies for this cookbok
+#defining dependancies for this cookbook
 depends "apache2"
 depends "mysql"
 depends "php"
